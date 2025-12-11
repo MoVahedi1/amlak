23f4bd05a0991c505bf8f549dea5c9a129da2967
MoVahedi1 <148808551+MoVahedi1@users.noreply.github.com>
2025-12-11 18:56:57 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // 禁用 Next.js 热重载，由 nodemon 处理重编译
+  
+  // تنظیمات نادیده گرفتن خطاها حذف شد تا بیلد امن‌تر باشد
+  
+  //禁用 Next.js 热重载，由 nodemon 处理重编译
   reactStrictMode: false,
-  eslint: {
-    // 构建时忽略ESLint错误
-    ignoreDuringBuilds: true,
-  },
+  
   images: {
     remotePatterns: [
       {
